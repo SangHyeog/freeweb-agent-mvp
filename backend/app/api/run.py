dcd208687b4c7203f14360897de501412df682df
DESKTOP-K856IDT\Nathan <nathan@superngine.com>
2026-01-13 18:34:47 +0900

    Day 6: run project code inside Docker sandbox

--- backend/app/api/run.py
+++ backend/app/api/run.py
@@ -1,9 +1,16 @@
 from fastapi import APIRouter
-from app.services.run_service import run_main_file
+from app.services.run_service import (run_main_file, run_main_file_docker)
 
 router = APIRouter(prefix="/run", tags=["run"])
 
 @router.post("")
+def run_project():
+    output = run_main_file_docker()
+    return {
+        "output": output
+    }
+
+@router.post("/main")
 def run_project():
     output = run_main_file()
     return {
--- backend/app/services/run_service.py
+++ backend/app/services/run_service.py
@@ -1,5 +1,7 @@
+import os
 import subprocess
 from app.core.config import MAIN_FILE
+from app.core.config import DEFAULT_PROJECT
 
 def run_main_file() -> str:
     """
@@ -7,6 +9,29 @@ def run_main_file() -> str:
     """
     process = subprocess.Popen(
         ["python", str(MAIN_FILE)],
+        stdout = subprocess.PIPE,
+        stderr = subprocess.STDOUT,
+        text = True,
+    )
+
+    output_lines = []
+    for line in process.stdout:
+        output_lines.append(line)
+
+    process.wait()
+    return "".join(output_lines)
+
+def run_main_file_docker() -> str:
+    """
+    Docker 컨테니어에서 main.py를 실행하고 stdout/stderr를 문자열로 반환
+    """
+    project_path = os.path.abspath(str(DEFAULT_PROJECT))
+    print("PROJECT PATH:", project_path)
+
+    cmd = ["docker", "run", "--rm", "-v", f"{project_path}:/app", "-w", "/app", "python:3.11-slim", "python", "main.py"]
+
+    process = subprocess.Popen(
+        cmd,
         stdout = subprocess.PIPE,
         stderr = subprocess.STDOUT,
         text = True,
